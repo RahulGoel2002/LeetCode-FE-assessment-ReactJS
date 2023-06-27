e0b481ccaab31d99402fe9e9d4e65c9f2c5bfe7d
Rahul Goel <59789240+RahulGoel2002@users.noreply.github.com>
2023-06-27 10:58:02 +0000

    caret jumping handled

--- src/InputPhoneField.js
+++ src/InputPhoneField.js
@@ -1,19 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { formatPhone, checkAlienInput } from "./Utils";
 
 export default function InputPhoneField() {
   const [inp, setInp] = useState("");
+  const [inpLen, setInpLen] = useState(0);
+  const [caretPos, setCaretPos] = useState(0);
+  const inpRef = useRef();
 
   const handleChange = (event) => {
+    setCaretPos(inpRef.current.selectionStart);
+
+    // console.log(caretPos);
     const value = event.target.value;
     const key = event.nativeEvent.data;
-    setInp(checkAlienInput(key) ? formatPhone(value) : inp);
+    const res = formatPhone(value);
+    console.log(caretPos, value.length);
+    if (caretPos === value.length - 1) {
+      console.log("yep");
+      setCaretPos((prev) => {
+        return prev + value.length - 1;
+      });
+    }
+    setInpLen(res.length);
+    setInp(checkAlienInput(key) ? res : inp);
   };
+
+  useEffect(() => {
+    console.log("Done afterwards");
+    // console.log(inpRef.current.value.length);
+    inpRef.current.selectionStart = caretPos;
+    inpRef.current.selectionEnd = caretPos;
+  }, [inp]);
 
   return (
     <div className="App">
       <form>
         <input
+          ref={inpRef}
           type="tel"
           value={inp}
           data-testid="inputfield"
